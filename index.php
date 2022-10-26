1039529e1b2898cae218396450b5dc3464723556
Herve KRASOWIAK <krasowiak.h@gmail.com>
2022-10-26 16:29:31 +0200

    connexion à la BDD kgb_evalphpMyadmin

--- index.php
+++ index.php
@@ -3,3 +3,11 @@ include "html\Header.html";
 include "html\Body.html";
 include "html\Footer.html";
 include "login.php";
+
+try {
+    $pdo_options[pdo::ATTR_ERRMODE] = pdo::ERRMODE_EXCEPTION;
+    echo 'connexion réussi';
+    $bdd = new pdo('mysql:host=localhost;dbname=kgb_eval', 'root', '', $pdo_options);
+} catch (Exception $ex) {
+    die("Erreur: " . $ex->getMessage());
+}
