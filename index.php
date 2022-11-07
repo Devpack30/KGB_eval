e30936753e83427576052891aca0c9764f4ec005
Herve KRASOWIAK <krasowiak.h@gmail.com>
2022-11-07 15:55:49 +0100

    reparation connexion base de données

--- index.php
+++ index.php
@@ -2,12 +2,14 @@
 include "html\Header.html";
 include "html\Body.html";
 include "html\Footer.html";
-include "connexion.php";
 
-try {
-    $pdo_options[pdo::ATTR_ERRMODE] = pdo::ERRMODE_EXCEPTION;
-    echo 'connexion réussi';
-    $bdd = new pdo('mysql:host=localhost;dbname=kgb_eval', 'root', '', $pdo_options);
-} catch (Exception $ex) {
-    die("Erreur: " . $ex->getMessage());
+
+$bdd = new pdo('mysql:host=localhost;dbname=kgb_eval', 'root', '',);
+
+
+if (isset($_POST['envoi'])) {
+    if (!empty($_POST['pseudo']) and !empty($_POST['mdp'])) {
+    } else {
+        echo "Veuillez remplir tout les champs !!!";
+    }
 }
