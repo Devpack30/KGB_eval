c5603a7cedf39ba06973c4a62268dc671c71db65
Herve KRASOWIAK <krasowiak.h@gmail.com>
2022-11-14 15:21:47 +0100

    mise en place alerte commende non valide connexion

--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?php
+//---------------------------------Injection des fichiers inclus dans PHP---------------------
 include "html\Header.html";
 include "html\Body.html";
 include "html\Footer.html";
 
-
+//-------------------------------- Connexion base de donnée ----------------------------------
 $bdd = new pdo('mysql:host=localhost;dbname=kgb_eval', 'root', '',);
 
-
+//-------------------------------- Formulaire de saisie + alerte de saisie non valide (JavaScript) ----------------------------------
 if (isset($_POST['envoi'])) {
     if (!empty($_POST['pseudo']) and !empty($_POST['mdp'])) {
     } else {
-        echo "Veuillez remplir tout les champs !!!";
+        echo " <script> alert('Attention vous êtes sur un site officiel de la Fédération de RUSSIE '); window.location='index.php'</script>";
     }
 }
